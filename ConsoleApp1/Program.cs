7f324142bc3486d7b821ff8f5d6b7651344c4ee1
shahrul <shahrul.hussain.shahul.hamid@intel.com>
2022-12-29 09:38:28 +0800

    cherry pick this

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,14 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("1");
-
         }
 
         void test()
         {
             Console.WriteLine("2");
-
         }
     }
 }
